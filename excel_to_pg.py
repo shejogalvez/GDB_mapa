2f1b9e2fe51c7a46791159989adc51b7cbb38310
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-08-20 23:27:22 -0400

    refactor, update

--- excel_to_pg.py
+++ excel_to_pg.py
@@ -1,58 +1,10 @@
 import pandas as pd
-import math
 import os
 
-WB_PATH = "2024 Muestra inventario colecciones MAPA.xlsx"
+WB_PATH = "2024 Muestra inventario colecciones MAPA (1).xlsx"
 
 dataframe = pd.read_excel(WB_PATH)
 dataframe = dataframe.reset_index()
-
-COLS = [
-    "numero_de_inventario",
-    "letra",
-    "ubicacion ",
-    "numero_de_registro_anterior",
-    "SURDOC",
-    "deposito",
-    "estante",
-    "caja_anterior",
-    "caja_actual",
-    "coleccion",
-    "clasificacion",
-    "conjunto",
-    "nombre_comun",
-    "titulo_o_nombre_atribuido",
-    "autor",
-    "filiacion_cultural",
-    "pais",
-    "localidad",
-    "tipologia",
-    "fecha_de_creacion",
-    "descripcion_fisica",
-    "marcas_o_inscripciones",
-    "alto_(cm)",
-    "ancho_(cm)",
-    "profundidad_(cm)",
-    "diametro_(cm)",
-    "peso_(grs)",
-    "tecnica",
-    "materialidad",
-    "funcion",
-    "contexto_historico",
-    "bibliografia",
-    "iconografia",
-    "notas",
-    "exposiciones",
-    "responsable",
-    "fecha_ultima_modificacion",
-    "avaluo",
-    "tipo",
-    "procedencia",
-    "fecha",
-    "estado_de_conservacion",
-    "intervenciones",
-    "Embalaje"
-]
 
 COL_PAIS = "pais"
 COL_LOCALIDAD = "localidad"
@@ -70,7 +22,7 @@ ATRIBUTOS_UBICACION = [
 ATRIBUTOS_COMPONENTES = [
     "nombre_comun", 
     "materialidad", 
-    #"peso_(grs)", 
+    "peso_grs", 
     "tecnica",
     "marcas_o_inscripciones",
     "descripcion_fisica",
@@ -101,18 +53,34 @@ ATRIBUTOS_PIEZAS = [
 
 
 # Function to count blank values (NaN or empty string) in a row
-def count_blank_values(row):
+def count_blank_values(row) -> int:
     return row.isna().sum() + (row == '').sum()
 
-def dict_to_pg_query(objects, *tags):
-    pg_query = ""
+def get_row_piece_id(row) -> str:
+    return f'P{row["numero_de_inventario"]}'
 
+def get_row_component_id(row) -> str:
+    return f'C{row["numero_de_inventario"]}{row["letra"] if not pd.isna(row["letra"]) else "a"}'
+
+def get_attributes(row, atributes_columns: list[str], replace_dict: dict = None) -> str:
+    result = ""
+    for att in atributes_columns:
+        element = row[att]
+        if pd.isna(element):
+            continue
+        if isinstance(element, str):
+            for old, new in replace_dict.items():
+                element = element.replace(old, new)
+            result += f' {att}:"{element}"'
+        else:
+            result += f' {att}:{element}'
+    return result
 
 pg_query = ""
-
 pg_edges = ""
 
-piezas_df = dataframe[['numero_de_inventario'] + ATRIBUTOS_PIEZAS]
+
+piezas_df = dataframe
 
 # Sort the DataFrame by the ID column and then by the count of blank values
 piezas_df['blank_count'] = piezas_df.apply(count_blank_values, axis=1)
@@ -121,39 +89,34 @@ piezas_df = piezas_df.sort_values(by=['numero_de_inventario', 'blank_count'])
 # Drop duplicates, keeping the row with the least number of blank columns
 drop_duplicate_piezas_df = piezas_df.drop_duplicates(subset='numero_de_inventario', keep='first').drop(columns='blank_count')
 
-# TODO: Filtrar piezas repetidas
+
 # TODO: Cambiar en mapa conceptual los campos de funcion, tipologia, iconografía, bibliografia(?), 
 for index, line in drop_duplicate_piezas_df.iterrows() :
-    # create pieza nodes
-    # print(line)
-    # for att in atributos_piezas:
-    #     print(type(line[att]), line[att], line[att] == 'NaN')
-    piece_node_line = f'P{line["numero_de_inventario"]} :pieza {" ".join([f"{att}:\"{str(line[att]).replace('\n', '').replace('"', "'").strip()}\"" for att in ATRIBUTOS_PIEZAS if not pd.isna(line[att])]) }\n'
+    id = get_row_piece_id(line)
+    attributes = get_attributes(line, ATRIBUTOS_PIEZAS, {'\n': '', '"': "'"})
+    piece_node_line = f'{id} :pieza {attributes}\n'
     pg_query += piece_node_line
 
 for index, line in dataframe.iterrows() :
     # create componentes nodes
-    id = f'C{line["numero_de_inventario"]}{line["letra"] if not pd.isna(line["letra"]) else "a"}'
-    comp_node_line = f'{id} :componente {" ".join([f"{att}:\"{str(line[att]).replace('\n', ' ').replace('"', "'").strip()}\"" for att in ATRIBUTOS_COMPONENTES if not pd.isna(line[att])]) }\n'
+    id = get_row_component_id(line)
+    attributes = get_attributes(line, ATRIBUTOS_COMPONENTES, {'\n': '', '"': "'"})
+    comp_node_line = f'{id} :componente {attributes}\n'
     pg_query += comp_node_line
 
-    pg_edges += f'P{line["numero_de_inventario"]}->{id} :compuesto_por\n'
+    pg_edges += f'{get_row_piece_id(line)}->{id} :compuesto_por\n'
 
 # extract distinct values from column pais
 pais_col_df = dataframe[COL_PAIS].drop_duplicates().dropna()
-pais_name_id = {name: f"pa{index}" for index, name in enumerate(pais_col_df)}
+pais_name_id = {name: f"pa{index+1}" for index, name in enumerate(pais_col_df)}
 for name, id in pais_name_id.items():
-    pg_query += f"{id} :pais name:{name}\n"
-# pg_query += " :pais\n".join(pais for pais in pais_col_df if pd.notna(pais))
-# pg_query += " :pais\n"
+    pg_query += f'{id} :pais name:"{name}"\n'
 
 # extract distinct values from column localidad
 loc_col_df = dataframe[COL_LOCALIDAD].drop_duplicates().dropna()
-loc_name_id = {name: f"loc{index}" for index, name in enumerate(loc_col_df)}
+loc_name_id = {name: f"loc{index+1}" for index, name in enumerate(loc_col_df)}
 for name, id in loc_name_id.items():
-    pg_query += f"{id} :localiudad name:{name}\n"
-# pg_query += " :localidad\n".join(loc for loc in loc_col_df if pd.notna(loc))
-# pg_query += " :localidad\n"
+    pg_query += f'{id} :localiudad name:"{name}"\n'
 
 
 deposito_name_id = dict()
@@ -179,7 +142,7 @@ for index, row in dataframe.iterrows():
     caja = row[COL_CAJA]
     if not pd.isna(caja):
         if caja not in caja_name_id:
-            c_id = f"C{len(caja_name_id) + 1}"
+            c_id = f"B{len(caja_name_id) + 1}"
             caja_name_id[caja] = c_id
         if estante in estante_name_id:
             ubicacion_dict[estante_name_id[estante]].add(caja_name_id[caja])
@@ -194,14 +157,6 @@ for name, id in caja_name_id.items():
     pg_query += f"{id} :caja :ubicacion name:\"{name}\"\n"
 
 
-# pg_query += " :deposito :ubicacion\n".join(str(x) for x in deposito_name_id if pd.notna(x))
-# pg_query += " :deposito :ubicacion\n"
-# pg_query += " :estante :ubicacion\n".join(str(x) for x in estante_name_id if pd.notna(x))
-# pg_query += " :estante :ubicacion\n"
-# pg_query += " :caja :ubicacion\n".join(str(x) for x in caja_name_id if pd.notna(x))
-# pg_query += " :caja :ubicacion\n"
-
-
 # parse relations between ubicacion nodes
 for contenedor, contenido in ubicacion_dict.items() :
     for elemento in contenido: 
@@ -210,7 +165,7 @@ for contenedor, contenido in ubicacion_dict.items() :
 # parse relations between ubicacion and components
 for index, line in dataframe.iterrows() :
     # obtain id
-    id = f'C{line["numero_de_inventario"]}{line["letra"] if not pd.isna(line["letra"]) else "a"}'
+    id = get_row_component_id(line)
     # obtain leaf node from ubicacion
     if not pd.isna(line[COL_CAJA]):
         ubicacion = caja_name_id[line[COL_CAJA]]
@@ -222,10 +177,64 @@ for index, line in dataframe.iterrows() :
         continue
     pg_edges += f"{id}->{ubicacion} :ubicado_en\n"
 
+# parse relations between pais and piezas
+for index, line in drop_duplicate_piezas_df.iterrows() :
+    # create pieza nodes
+    id = f'P{line["numero_de_inventario"]}'
+    if not pd.isna(line[COL_PAIS]):
+        id2 = pais_name_id[line[COL_PAIS]]
+    pg_edges += f"{id}->{id2} :de_pais\n"
+
+# parse relations between loc and piezas
+for index, line in drop_duplicate_piezas_df.iterrows() :
+    # create pieza nodes
+    id = f'P{line["numero_de_inventario"]}'
+    if not pd.isna(line[COL_LOCALIDAD]):
+        id2 = loc_name_id[line[COL_LOCALIDAD]]
+    pg_edges += f"{id}->{id2} :de_localidad\n"
+
+
+# Detrminar forma de cada pieza y linkear
+
+ATRIBUTOS_FORMAS = [
+    "alto_cm",
+    "ancho_cm",
+    "profundidad_cm",
+    "diametro_cm"
+]
+for index, row in dataframe.iterrows():
+    alto =     ATRIBUTOS_FORMAS[0]
+    diametro = ATRIBUTOS_FORMAS[3]
+    if not pd.isna(row[diametro]):
+        d_id = f"D{len(deposito_name_id) + 1}"
+        ubicacion_dict[d_id] = set()
+        deposito_name_id[deposito] = d_id
+        if not pd.isna(row[alto]):
+            forma = "cilindro"
+        else:
+            forma = "esfera"
+    else:
+        blanks = count_blank_values(row[ATRIBUTOS_FORMAS])
+        # print(f"{blanks=}\n", row[ATRIBUTOS_FORMAS])
+        match blanks:
+            case 1:
+                forma = "prisma"
+            case 2:
+                forma = "plano"
+            case 3:
+                forma = "cuerda"
+            case _:
+                continue
+    atributos = get_attributes(row, ATRIBUTOS_FORMAS)
+    self_id = f"F{index}"
+    pg_query += f"{self_id} :forma :{forma} {atributos}\n"
+    pg_edges += f"{get_row_component_id(row)}->{self_id} :tiene_forma\n"
 pg_query += pg_edges
 
 # Open the file in write mode ('w') and write the string to it
 with open(os.path.join("C:/Users/shejo/Documents/GitHub/MillenniumDB/data", "MAPA-DB.txt"), "w", encoding="utf-8") as file:
+    file.write(pg_query)
+with open(os.path.join("MAPADB.txt"), "w", encoding="utf-8") as file:
     file.write(pg_query)
 
 print("File created successfully!")
