3ad340170c06c8192e0b555232e38b56a16a793b
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-10-03 03:49:58 -0300

    add user data to db
    also add salt to password hashing

--- backend/user.py
+++ backend/user.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Form 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
+from passlib.pwd import genword
 from pydantic import BaseModel
 from typing import List, Optional, Annotated
 from jose import jwt, JWTError
 from datetime import timedelta, datetime
 import os
+from db import db
 
 router = APIRouter()
 
@@ -16,7 +18,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # OAuth2 scheme for token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# In-memory database (for simplicity)
+# In-memory database (for simplicity) TODO: delete and connect to actual database
 users_db = {
     "user1": {
         "username": "user1",
@@ -43,20 +45,24 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+    salt: Optional[str] = "" #TODO: not optional
 
 # Helper functions to authenticate and verify users
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(username: str):
+    user = db.get_user(username)
+    # if username not in users_db:
+    #     return None
+    # user = users_db[username]
+    if user:
+        return UserInDB(**user)
     return None
 
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
-    if not user or not verify_password(password, user.hashed_password):
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user or not verify_password(password + user.salt, user.hashed_password):
         return False
     return user
 
@@ -100,14 +106,13 @@ def enconde_token(payload: dict, expires_delta: timedelta) -> str:
     return token
 
 def decode_token(token: str):
-    print(f"{token=}")
     data = jwt.decode(token, SECRET, algorithms=["HS256"])
-    user = get_user(users_db, data["username"])
+    user = get_user(data["username"])
     return user
 
 @router.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user = authenticate_user(users_db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -117,6 +122,15 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     token = enconde_token({"username": user.username}, expires_delta= timedelta(minutes=120))
     return {"access_token": token, "token_type": "bearer"}
 
+#TODO: test
+@router.post("/user", dependencies=[Depends(get_admin_permission_user)])
+async def add_user(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    salt = genword(length=15, charset="ascii_72")
+    hashed_password = pwd_context.hash(password + salt)
+    db.create_user(username, hashed_password, salt, "admin")
+    return {"username": username}
+
+## TEST ROUTES, TODO: DELETE
 # Open route, anyone can access
 @router.get("/user")
 async def root():
