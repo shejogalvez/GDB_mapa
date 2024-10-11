8e9acc3a725538bdbc4ed7396555575e21c59b70
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-10-11 13:43:57 -0300

    clean excel parser
    - warns when component have same id's
    - create main functiion to script
    - save dates as date instead of str

--- excel_to_neo4j_csv.py
+++ excel_to_neo4j_csv.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import os
-from backend import db
+import sys
+from neo4j import GraphDatabase
 
-WB_PATH = "2024 Inventario Colecciones MAPA-PCMAPA (1).xlsx"
+# Set up the connection details
+uri = f"bolt://{ os.getenv("NEO4J_HOSTNAME", "localhost") }:7687"  # Bolt URI of your Neo4j server
+ 
+username, password = os.getenv("NEO4J_AUTH").split('/')  # Your Neo4j username
 
-dataframe = pd.read_excel(WB_PATH)
-dataframe = dataframe.reset_index()
-# cleans spaces in string fields
-dataframe = dataframe.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-
+# Nombres de columnas excel que contine
 COL_PAIS      = "pais"
 COL_LOCALIDAD = "localidad"
 COL_CULTURA   = "filiacion_cultural"
 COL_DEPOSITO  = "deposito"
 COL_ESTANTE   = "estante"
 COL_CAJA      = "caja_actual"
-COL_EXPO   = "exposiciones"
-
+COL_EXPO      = "exposiciones"
 
 ATRIBUTOS_COMPONENTES = [
     "nombre_comun", 
@@ -26,16 +25,21 @@ ATRIBUTOS_COMPONENTES = [
     "tecnica",
     "marcas_o_inscripciones",
     "descripcion_fisica",
+    'descripcion_col',
+    'descripcion_cr',
     "tipologia",
     "funcion",
     "iconografia",
-    "estado_de_conservacion"
+    "estado_genral_de_conservacion",
+    'responsable_coleccion',
+    'fecha_ultima_modificacion'
 ]
 
 ATRIBUTOS_PIEZAS = [
     #"numero_de_inventario", #ID
     "numero_de registro_anterior",
     "coleccion",
+    "SURDOC",
     "clasificacion",
     "conjunto",
     "autor",
@@ -81,14 +85,6 @@ def column_to_name_id_dict(df: pd.DataFrame, column_name: str, prefix="A"):
     column_data = df[column_name].drop_duplicates().dropna()
     return {name: f"{prefix}{index+1}" for index, name in enumerate(column_data)}
 
-
-def column_to_dataframe(df: pd.DataFrame, column_name, prefix="A") -> pd.DataFrame:
-    data = {
-        "id" : [f"{prefix}{index+1}" for _ in df[column_name].dropna()],
-        "name" : list(df[column_name].dropna())
-    }
-    return pd.DataFrame(data)
-
 def insert_id_column_to_df(df: pd.DataFrame, column_name: str, name_id_dict: dict, column_id_name="id", loc = 0) -> None:
     id_data = [None if pd.isna(x) else name_id_dict[x] for x in df[column_name]]
     df.insert(loc, column_id_name, id_data)
@@ -102,6 +98,8 @@ def multi(df: pd.DataFrame, column_name, prefix):
     column_id_name = f"{column_name}_id"
     insert_id_column_to_df(df, column_name, name_id_dict, column_id_name)
     result_df = df[[column_name, column_id_name]].drop_duplicates().dropna()
+    result_df = result_df[result_df[column_name] != ""]
+    print(result_df)
     dataframe_to_csv(result_df, f"{column_name}.csv")
 
 def add_reference_and_create_table(df: pd.DataFrame, column_name, name_id_dict):
@@ -110,158 +108,160 @@ def add_reference_and_create_table(df: pd.DataFrame, column_name, name_id_dict):
     result_df = df[[column_name, column_id_name]].drop_duplicates()
     dataframe_to_csv(result_df, f"{column_name}.csv")
 
-# Initialize an empty dictionary to store ids where differences are found per column
-different_values_ids = {col: [] for col in ATRIBUTOS_PIEZAS}
 
-# Group by 'id' column
-grouped = dataframe.groupby('numero_de_inventario')
+def main(WB_PATH):
+    dataframe = pd.read_excel(WB_PATH)
+    dataframe = dataframe.reset_index()
+    # cleans spaces in string fields
+    dataframe = dataframe.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-# Iterate through each group
-for group_id, group in grouped:
-    for col in ATRIBUTOS_PIEZAS:
-        # Check if the values in the current column vary within the group
-        if group[col].nunique(True) > 1:
-            # If they do, add the id to the corresponding list for that column
-            different_values_ids[col].append(group_id)
+    piezas_df = dataframe
 
-print(different_values_ids)
+    # Sort the DataFrame by the ID column and then by the count of blank values
+    piezas_df['blank_count'] = piezas_df.apply(count_blank_values, axis=1)
+    piezas_df = piezas_df.sort_values(by=['numero_de_inventario', 'blank_count'])
 
-piezas_df = dataframe
+    # Drop duplicates, keeping the row with the least number of blank columns
+    drop_duplicate_piezas_df = piezas_df.drop_duplicates(subset='numero_de_inventario', keep='first').drop(columns='blank_count')
 
-# Sort the DataFrame by the ID column and then by the count of blank values
-piezas_df['blank_count'] = piezas_df.apply(count_blank_values, axis=1)
-piezas_df = piezas_df.sort_values(by=['numero_de_inventario', 'blank_count'])
+    # add id to single components
+    component_id_column = [get_row_component_id(row) for _, row in dataframe.iterrows()]
+    dataframe.insert(1, "component_id", component_id_column)
+    duplicated_components_id = dataframe[dataframe[['component_id']].duplicated() == True]["component_id"]
+    if (len(duplicated_components_id) > 0):
+        print("Existen pares de componentes/letra repetidos, datos no se van a cargar correctamente\n", duplicated_components_id)
 
-# Drop duplicates, keeping the row with the least number of blank columns
-drop_duplicate_piezas_df = piezas_df.drop_duplicates(subset='numero_de_inventario', keep='first').drop(columns='blank_count')
+    deposito_name_id = dict()
+    estante_name_id = dict()
+    caja_name_id = dict()
+    ubicacion_dict = dict()
+    for index, row in dataframe.iterrows():
+        deposito = row[COL_DEPOSITO]
+        if not pd.isna(deposito) and deposito not in deposito_name_id:
+            d_id = f"D{len(deposito_name_id) + 1}"
+            ubicacion_dict[d_id] = set()
+            deposito_name_id[deposito] = d_id
 
-# add id to single components
-component_id_column = [get_row_component_id(row) for _, row in dataframe.iterrows()]
-dataframe.insert(1, "component_id", component_id_column)
+        estante = row[COL_ESTANTE]
+        if not pd.isna(estante):
+            if estante not in estante_name_id:
+                e_id = f"E{len(estante_name_id) + 1}"
+                ubicacion_dict[e_id] = set()
+                estante_name_id[estante] = e_id
+            if deposito in deposito_name_id:
+                ubicacion_dict[deposito_name_id[deposito]].add(estante_name_id[estante])
 
+        caja = row[COL_CAJA]
+        if not pd.isna(caja):
+            if caja not in caja_name_id:
+                c_id = f"B{len(caja_name_id) + 1}"
+                caja_name_id[caja] = c_id
+            if estante in estante_name_id:
+                ubicacion_dict[estante_name_id[estante]].add(caja_name_id[caja])
+            elif deposito in deposito_name_id:
+                ubicacion_dict[deposito_name_id[deposito]].add(caja_name_id[caja])
+    ubicacion_df = pd.concat([
+        pd.DataFrame(list(deposito_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,deposito'),
+        pd.DataFrame(list(estante_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,estante'),
+        pd.DataFrame(list(caja_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,caja')
+    ])
+    dataframe_to_csv(ubicacion_df, 'ubicaciones.csv')
+    # dataframe_to_csv(pd.DataFrame(list(deposito_name_id.items()), columns=['id', 'value']), "depositos.csv")
+    # dataframe_to_csv(pd.DataFrame(list(estante_name_id.items()), columns=['id', 'value']) , "estantes.csv")
+    # dataframe_to_csv(pd.DataFrame(list(caja_name_id.items()), columns=['id', 'value'])    , "cajas.csv")
 
-deposito_name_id = dict()
-estante_name_id = dict()
-caja_name_id = dict()
-ubicacion_dict = dict()
-for index, row in dataframe.iterrows():
-    deposito = row[COL_DEPOSITO]
-    if not pd.isna(deposito) and deposito not in deposito_name_id:
-        d_id = f"D{len(deposito_name_id) + 1}"
-        ubicacion_dict[d_id] = set()
-        deposito_name_id[deposito] = d_id
+    # parse relations between ubicacion nodes
+    contenedor_list = []
+    for contenedor, contenido in ubicacion_dict.items():
+        for elemento in contenido: 
+            contenedor_list.append((contenedor, elemento))
+    dataframe_to_csv(pd.DataFrame(list(contenedor_list), columns=['id_contenedor', 'id_contenido']), "ubicaciones_rel.csv")
 
-    estante = row[COL_ESTANTE]
-    if not pd.isna(estante):
-        if estante not in estante_name_id:
-            e_id = f"E{len(estante_name_id) + 1}"
-            ubicacion_dict[e_id] = set()
-            estante_name_id[estante] = e_id
-        if deposito in deposito_name_id:
-            ubicacion_dict[deposito_name_id[deposito]].add(estante_name_id[estante])
-
-    caja = row[COL_CAJA]
-    if not pd.isna(caja):
-        if caja not in caja_name_id:
-            c_id = f"B{len(caja_name_id) + 1}"
-            caja_name_id[caja] = c_id
-        if estante in estante_name_id:
-            ubicacion_dict[estante_name_id[estante]].add(caja_name_id[caja])
-        elif deposito in deposito_name_id:
-            ubicacion_dict[deposito_name_id[deposito]].add(caja_name_id[caja])
-ubicacion_df = pd.concat([
-    pd.DataFrame(list(deposito_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,deposito'),
-    pd.DataFrame(list(estante_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,estante'),
-    pd.DataFrame(list(caja_name_id.items()), columns=['value', 'id']).assign(labels='ubicacion,caja')
-])
-dataframe_to_csv(ubicacion_df, 'ubicaciones.csv')
-# dataframe_to_csv(pd.DataFrame(list(deposito_name_id.items()), columns=['id', 'value']), "depositos.csv")
-# dataframe_to_csv(pd.DataFrame(list(estante_name_id.items()), columns=['id', 'value']) , "estantes.csv")
-# dataframe_to_csv(pd.DataFrame(list(caja_name_id.items()), columns=['id', 'value'])    , "cajas.csv")
-
-# parse relations between ubicacion nodes
-contenedor_list = []
-for contenedor, contenido in ubicacion_dict.items():
-    for elemento in contenido: 
-        contenedor_list.append((contenedor, elemento))
-dataframe_to_csv(pd.DataFrame(list(contenedor_list), columns=['id_contenedor', 'id_contenido']), "ubicaciones_rel.csv")
-
-# parse relations between ubicacion and components
-componenteId_ubicacion = []
-for index, line in dataframe.iterrows():
-    # obtain id
-    id = get_row_component_id(line)
-    # obtain leaf node from ubicacion
-    if not pd.isna(line[COL_CAJA]):
-        ubicacion = caja_name_id[line[COL_CAJA]]
-    elif not pd.isna(line[COL_ESTANTE]):
-        ubicacion = estante_name_id[line[COL_ESTANTE]]
-    elif not pd.isna(line[COL_DEPOSITO]):
-        ubicacion = deposito_name_id[line[COL_DEPOSITO]]
-    else:
-        continue
-    componenteId_ubicacion.append((id, ubicacion))
-dataframe_to_csv(pd.DataFrame(list(componenteId_ubicacion), columns=['id_componente', 'id_ubicacion']), "ubicacion_objetos.csv")
-
-
-# Detrminar forma de cada pieza y linkear
-
-ATRIBUTOS_FORMAS = [
-    "alto_o_largo_(cm)",
-    "ancho_(cm)",
-    "profundidad_(cm)",
-    "diametro_(cm)",
-    'espesor_(mm)'
-]
-formas_propiedades = []
-for index, row in dataframe.iterrows():
-    alto =     ATRIBUTOS_FORMAS[0]
-    diametro = ATRIBUTOS_FORMAS[3]
-    if not pd.isna(row[diametro]):
-        d_id = f"D{len(deposito_name_id) + 1}"
-        ubicacion_dict[d_id] = set()
-        deposito_name_id[deposito] = d_id
-        if not pd.isna(row[alto]):
-            forma = "cilindro"
+    # parse relations between ubicacion and components
+    componenteId_ubicacion = []
+    for index, line in dataframe.iterrows():
+        # obtain id
+        id = get_row_component_id(line)
+        # obtain leaf node from ubicacion
+        if not pd.isna(line[COL_CAJA]):
+            ubicacion = caja_name_id[line[COL_CAJA]]
+        elif not pd.isna(line[COL_ESTANTE]):
+            ubicacion = estante_name_id[line[COL_ESTANTE]]
+        elif not pd.isna(line[COL_DEPOSITO]):
+            ubicacion = deposito_name_id[line[COL_DEPOSITO]]
         else:
-            forma = "esfera"
-    else:
-        blanks = count_blank_values(row[ATRIBUTOS_FORMAS])
-        # print(f"{blanks=}\n", row[ATRIBUTOS_FORMAS])
-        match blanks:
-            case 1:
-                forma = "prisma"
-            case 2:
-                forma = "plano"
-            case 3:
-                forma = "cuerda"
-            case _:
-                continue
-    atributos = list(row[ATRIBUTOS_FORMAS])
-    self_id = f"F{index}"
-    atributos.extend([self_id, get_row_component_id(row), f"forma,{forma}"])
-    formas_propiedades.append(atributos)
-
-# export formas info
-dataframe_to_csv(pd.DataFrame(list(formas_propiedades), 
-                                columns=ATRIBUTOS_FORMAS + ['id', 'id_componente','labels']), 
-                                f"forma.csv"
-                            )
-multi(drop_duplicate_piezas_df, COL_PAIS, "PA")
-multi(drop_duplicate_piezas_df, COL_LOCALIDAD, "LOC")
-multi(drop_duplicate_piezas_df, COL_CULTURA, "CUL")
-dataframe_to_csv(drop_duplicate_piezas_df, "piezas.csv")
-
-dataframe.drop(columns=dataframe.columns[0], axis=1, inplace=True)
-dataframe_to_csv(dataframe, "all.csv")
+            continue
+        componenteId_ubicacion.append((id, ubicacion))
+    dataframe_to_csv(pd.DataFrame(list(componenteId_ubicacion), columns=['id_componente', 'id_ubicacion']), "ubicacion_objetos.csv")
 
 
-print("Files created successfully!")
+    # Detrminar forma de cada pieza y linkear
+
+    ATRIBUTOS_FORMAS = [
+        "alto_o_largo_(cm)",
+        "ancho_(cm)",
+        "profundidad_(cm)",
+        "diametro_(cm)",
+        'espesor_(mm)'
+    ]
+    formas_propiedades = []
+    for index, row in dataframe.iterrows():
+        alto =     ATRIBUTOS_FORMAS[0]
+        diametro = ATRIBUTOS_FORMAS[3]
+        if not pd.isna(row[diametro]):
+            d_id = f"D{len(deposito_name_id) + 1}"
+            ubicacion_dict[d_id] = set()
+            deposito_name_id[deposito] = d_id
+            if not pd.isna(row[alto]):
+                forma = "cilindro"
+            else:
+                forma = "esfera"
+        else:
+            blanks = count_blank_values(row[ATRIBUTOS_FORMAS])
+            # print(f"{blanks=}\n", row[ATRIBUTOS_FORMAS])
+            match blanks:
+                case 1:
+                    forma = "prisma"
+                case 2:
+                    forma = "plano"
+                case 3:
+                    forma = "cuerda"
+                case _:
+                    continue
+        atributos = list(row[ATRIBUTOS_FORMAS])
+        self_id = f"F{index}"
+        atributos.extend([self_id, get_row_component_id(row), f"forma,{forma}"])
+        formas_propiedades.append(atributos)
+
+    # export formas info
+    dataframe_to_csv(pd.DataFrame(list(formas_propiedades), 
+                                    columns=ATRIBUTOS_FORMAS + ['id', 'id_componente','labels']), 
+                                    f"forma.csv"
+                                )
+    multi(drop_duplicate_piezas_df, COL_PAIS, "PA")
+    multi(drop_duplicate_piezas_df, COL_LOCALIDAD, "LOC")
+    multi(drop_duplicate_piezas_df, COL_CULTURA, "CUL")
+    dataframe_to_csv(drop_duplicate_piezas_df, "piezas.csv")
+
+    dataframe.drop(columns=dataframe.columns[0], axis=1, inplace=True)
+    dataframe_to_csv(dataframe, "all.csv")
 
 
-with open("import.cypher") as file:
-    queries = file.read().split(";\n")
-    for query in queries:
-        db.run_query(query)
+    print("Files created successfully!")
 
-print("data imported to neo4j")
+
+    with open("import.cypher") as file:
+        queries = file.read().split(";\n")
+        with GraphDatabase.driver(uri, auth=(username, password)) as driver:
+            for query in queries:
+                    driver.execute_query(query, database_="neo4j")
+
+    print("data imported to neo4j")
+
+if __name__ == "__main__":
+    args = sys.argv
+    print(args)
+    if len(args) < 2:
+        print("ingresar ruta de archivo excel a cargar")
+        exit(-1)
+    main(args[1])
--- import.cypher
+++ import.cypher
@@ -1,9 +1,19 @@
 CREATE CONSTRAINT pieza_pk IF NOT EXISTS FOR (n:piezas) REQUIRE n.id IS UNIQUE;
+CREATE CONSTRAINT componente_pk IF NOT EXISTS FOR (n:componente) REQUIRE n.id IS UNIQUE;
 CREATE CONSTRAINT pais_pk IF NOT EXISTS FOR (n:pais) REQUIRE n.name IS UNIQUE;
 CREATE CONSTRAINT localidad_pk IF NOT EXISTS FOR (n:localidad) REQUIRE n.name IS UNIQUE;
 
+LOAD CSV WITH HEADERS FROM 'file:///pais.csv' AS row
+MERGE (:pais {id: row.pais_id, name: row.pais});
+
+LOAD CSV WITH HEADERS FROM 'file:///localidad.csv' AS row
+MERGE (:localidad {name: row.localidad});
+
+LOAD CSV WITH HEADERS FROM 'file:///filiacion_cultural.csv' AS row
+MERGE (:cultura {name: row.filiacion_cultural});
+
 LOAD CSV WITH HEADERS FROM 'file:///all.csv' AS row
-MERGE (n :componente {id: row.component_id})
+CREATE (n :componente {id: row.component_id})
 SET n.nombre_comun =           row.nombre_comun,
     n.nombre_especifico =      row.nombre_especifico,
     n.materialidad =           row.materialidad,
@@ -14,7 +24,11 @@ SET n.nombre_comun =           row.nombre_comun,
     n.tipologia =              row.tipologia,
     n.funcion =                row.funcion,
     n.iconografia =            row.iconografia,
-    n.estado_de_conservacion = row.estado_de_conservacion;
+    n.estado_de_conservacion = row.estado_genral_de_conservacion,
+    n.descripcion_col =        row.descripcion_col,
+    n.descripcion_cr =         row.descripcion_cr,
+    n.responsable_coleccion =  row.responsable_coleccion,
+    n.fecha_ultima_modificacion = CASE WHEN n.fecha_ultima_modificacion IS NULL THEN NULL ELSE date(datetime({epochmillis: apoc.date.parse(row.fecha_ultima_modificacion, "ms", "dd/MM/yyyy")})) END;
     
 LOAD CSV WITH HEADERS FROM 'file:///forma.csv' AS row
 WITH row, SPLIT(row.labels, ',') AS labeles
@@ -27,15 +41,6 @@ CALL {
 WITH a, row
 MATCH (b:componente {id: row.id_componente})
 CREATE (a)<-[:tiene_forma]-(b);
-
-LOAD CSV WITH HEADERS FROM 'file:///pais.csv' AS row
-CREATE (:pais {id: row.pais_id, name: row.pais});
-
-LOAD CSV WITH HEADERS FROM 'file:///localidad.csv' AS row
-MERGE (:localidad {name: row.localidad});
-
-LOAD CSV WITH HEADERS FROM 'file:///filiacion_cultural.csv' AS row
-CREATE (:cultura {name: row.filiacion_cultural});
 
 LOAD CSV WITH HEADERS FROM 'file:///ubicaciones.csv' AS row
 WITH row, SPLIT(row.labels, ',') AS labeles
@@ -63,7 +68,8 @@ SET n.numero_de_registro_anterior = row.`numero_de registro_anterior`,
     n.avaluo =                      row.avaluo,
     n.procedencia =                 row.tipo,
     n.donante =                     row.procedencia,
-    n.fecha_ingreso =               row.fecha;
+    n.fecha_ingreso =               CASE WHEN n.fecha_ingreso IS NULL THEN NULL ELSE date(datetime({epochmillis: apoc.date.parse(row.fecha_ingreso, "ms", "dd/MM/yyyy")})) END,
+    n.fecha_ingreso_text =          row.fecha_ingreso;
 
 
 LOAD CSV WITH HEADERS FROM 'file:///piezas.csv' AS row
@@ -83,5 +89,6 @@ LOAD CSV WITH HEADERS FROM 'file:///all.csv' AS row
 MATCH (a:pieza {id: row.numero_de_inventario}), (b:componente {id: row.component_id})
 CREATE (a)-[:compuesto_por ]->(b);
 
-LOAD CSV WITH HEADERS FROM 'file:///exposiciones.csv' AS row
-CREATE (:exposicion {id: row.exposiciones_id, name: row.exposiciones});
+//TBD
+//LOAD CSV WITH HEADERS FROM 'file:///exposiciones.csv' AS row
+//CREATE (:exposicion {id: row.exposiciones_id, name: row.exposiciones});
