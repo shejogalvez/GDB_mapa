c4a8213a02898821000ce4396dcceaa4a35dff1a
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-10-18 23:16:48 -0300

    add expired token logic

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createMemoryHistory, createRouter } from 'vue-router';
 import axios from 'axios';
+import { jwtDecode } from "jwt-decode";
 import LoginPage from '../components/LoginPage.vue';
 import DataTable from '../components/DataTable.vue';  // Assume you have a Dashboard component
 
@@ -8,34 +9,44 @@ const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: DataTable,
-    beforeEnter: (to, from, next) => {
-      const token = localStorage.getItem('token');
-      console.log(token)
-      if (!token) {
-        next('/login');  // Redirect to login if not authenticated
-      } else {
-        next();  // Proceed to dashboard
-      }
-    }
+    component: DataTable
   },
   { path: '/:pathMatch(.*)*', name: "nn",
     beforeEnter: (to, from, next) => {
-        const token = localStorage.getItem('token');
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-        if (!token) {
-          next('/login');  // Redirect to login if not authenticated
-        } else {
-          next('/dashboard');  // Proceed to dashboard
-        }
+      next('/dashboard');  // Proceed to dashboard
     }
-  }  // Redirect any unknown paths to login
+  }  // Redirect any unknown paths to main page
 ];
 
 const router = createRouter({
     history: createMemoryHistory(),
     routes,
 });
-  
+
+// Checks if token is valid, else cleans localstorage
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  if (!token) return false
+  if (jwtDecode(token).exp < Date.now() / 1000) {
+    console.log("TOKEN EXPIRED")
+    localStorage.clear();
+    return false
+  }
+  return true
+}
+
+router.beforeEach(async (to, from) => {
+  if (
+    !isAuthenticated() &&
+    to.name !== 'Login'
+  ) {
+    // redirect the user to the login page
+    return { name: 'Login' }
+  }
+  else {
+    const token = localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  }
+})
 
 export default router;
