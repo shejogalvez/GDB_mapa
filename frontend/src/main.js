53b6d16d0037f11e50b2542eeba42e23b6f53723
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-10-23 04:06:03 -0300

    install vuetify and pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,14 @@
+// Plugins
+import { registerPlugins } from '@/plugins'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import router from './router/index'
 import App from './App.vue'
 
-createApp(App)
-.use(router)
+const app = createApp(App)
+registerPlugins(app)
+
+app.use(router)
 .mount('#app')
