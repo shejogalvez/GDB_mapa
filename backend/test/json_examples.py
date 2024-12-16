88d282aa1a156cd6fd1304b7e118677b2c58fe6f
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2024-12-16 10:36:46 -0300

    add api tests

--- backend/test/json_examples.py
+++ backend/test/json_examples.py
@@ -1,55 +1,26 @@
 import json
 
 CREATE_PIECE_BODY = {
-    "properties": {
-      "coleccion": "string",
-      "numero_de_registro_anterior": "string"
-    },
-    "connected_nodes": [
-      {
-        "node_id": "Chile",
-        "properties": {
-        },
-        "relation_label": "de_pais",
-        "node_label": "pais",
-        "id_key": "name",
-      },
-      {
-        "node_id": "Mapuche",
-        "properties": {
-        },
-        "relation_label": "de_cultura",
-        "node_label": "cultura",
-        "id_key": "name",
-      }
-    ],
+  "properties": {
     "id": "12345",
-    "components": [
-      {
-        "properties": {
-          "funcion": "Simbólica-utilitaria",
-          "nombre_comun": "Alfiler",
-          "nombre_especifico": "Ketawue",
-          "alto": "7"
-        },
-        "connected_nodes": [
-          {
-            "node_id": "Caja Fuerte",
-            "properties": {
-            },
-            "relation_label": "ubicacion_componente",
-            "node_label": "ubicacion",
-            "id_key": "name",
-          },
-        ]
-      }
-    ]
-  }
-CREATE_PIECE_FORM = { 
-  "node_create": json.dumps(CREATE_PIECE_BODY)
-}
-
-CREATE_COMPONENT_BODY = {
+    "coleccion": "string",
+    "numero_de_registro_anterior": "string",
+    "fecha_de_creacion": "siglo XX",
+  },
+  "connected_nodes": [
+    {
+      "node_id": "Chile",
+      "relation_label": "de_pais",
+      "node_label": "pais",
+      "id_key": "name",
+    },
+    {
+      "node_id": "Mapuche",
+      "relation_label": "de_cultura",
+      "node_label": "cultura",
+      "id_key": "name",
+    }
+  ],
   "components": [
     {
       "properties": {
@@ -59,12 +30,103 @@ CREATE_COMPONENT_BODY = {
         "alto": "7"
       },
       "connected_nodes": [
-          ""
+        {
+          "node_id": "Caja Fuerte",
+          "node_label": "ubicacion",
+          "id_key": "name",
+        },
       ]
     }
   ]
 }
-CREATE_COMPONENT_BODY
+  
+CREATE_PIECE_FORM = { 
+  "node_create": json.dumps(CREATE_PIECE_BODY),
+}
+
+CREATE_COMPONENT_BODY = {
+    "properties": {
+      "funcion": "Simbólica-utilitaria",
+      "nombre_comun": "Alfiler",
+      "nombre_especifico": "Ketawue",
+      "alto": "7"
+    },
+    "connected_nodes": [
+    ]
+}
+
+CREATE_PIECE_BODY_2 = {
+  "properties": {
+    "id": "12346",
+    "conjunto": "Bola de rompecabezas con pilar",
+    "coleccion": "Colección China",
+    "fecha_de_creacion": "ca. 1953",
+    "notas_investigacion": "Se puede atribuir el ingreso de esta pieza a la colección por medio de la donación realizada por la delegación china encabezada por Li I Mang en 1953 durante el Congreso Continental de la Cultura debido a su precencia en el listado de objetos de la exposición de 1955.",
+    "procedencia": "Donación",
+    "dondante": "Li I  Mang, representante de la delegación china dona una colección al MAPA luego del Congreso Continental de la Cultura, donde se incluye esta pieza",
+    "fecha_ingreso": "1953",
+  },
+  "connected_nodes": [
+    {
+      "node_id": "China",
+      "node_label": "pais",
+      "id_key": "name",
+    },
+    {
+      "node_id": "Guangzhou; Provincia de Guangdong",
+      "node_label": "localidad",
+      "id_key": "name",
+    }
+  ],
+  "components": [
+    {
+      "properties": {
+        "id": "C12346a",
+        "funcion": "Simbólica-utilitaria",
+        "nombre_comun": "Bola de rompecabezas",
+        "nombre_especifico": "El árbol de la vida",
+        "funcion": "Ornamental",
+        "tipologia": "Asta",
+      },
+      "connected_nodes": [
+        {
+          "node_label": "forma",
+          "method": "MERGE",
+          "properties": {
+              "profundidad": 1,
+              "diametro": 1,
+              "peso": 1
+          }
+        },
+      ]
+    },
+    {
+      "properties": {
+        "id": "C12346b",
+        "funcion": "Simbólica-utilitaria",
+        "nombre_comun": "Parte del pilar",
+        "nombre_especifico": "El árbol de la vida",
+        "funcion": "Ornamental",
+        "tipologia": "Asta",
+      }
+    },
+    {
+      "properties": {
+        "id": "C12346c",
+        "funcion": "Simbólica-utilitaria",
+        "nombre_comun": "Parte del pilar",
+        "nombre_especifico": "El árbol de la vida",
+        "funcion": "Ornamental",
+        "tipologia": "Asta",
+      }
+    },
+  ]
+}
+
+CREATE_COMPONENT_FORM = {
+    "node_create": json.dumps(CREATE_COMPONENT_BODY),
+    "piece_id": "12345"
+}
 
 LOGIN_ADMIN = {
     "username": "admin",
@@ -87,4 +149,36 @@ LOGIN_USER = {
 WRONG_USER = {
     "username": "test_reader",
     "password": "wrong_password"
+}
+
+FILTERS_1 = {
+  "pieza": [
+    {
+      "key": "string",
+      "operation": "<",
+      "val": "string"
+    },
+    {
+      "key": "string",
+      "operation": ">",
+      "val": "string"
+    }
+  ],
+  "pais": [
+    {
+      "key": "string",
+      "operation": "=",
+      "val": "string"
+    }
+  ]
+}
+
+FILTERS_0 = {
+    "pieza": [
+      {
+        "key": "id",
+        "operation": "=",
+        "val": "12345"
+      }
+    ]
 }
--- backend/test_main.py
+++ backend/test_main.py
@@ -1,19 +1,24 @@
 import db
-from main import app
+from main import app, UPLOAD_DIR
 from fastapi.testclient import TestClient
 from test.json_examples import *
+import os, os.path
 
-# TEST_DB_NAME = "test"
+TEST_DB_NAME = "test"
 
-# def init():
-#     init_test_db_query = f"CREATE DATABASE {TEST_DB_NAME} IF NOT EXISTS"
-#     db.run_query(init_test_db_query, database_ = None)
+def init():
+    init_test_db_query = f"CREATE DATABASE {TEST_DB_NAME} IF NOT EXISTS"
+    db.run_query(init_test_db_query, database_ = None)
 
 ## TODO: add new database for testing
 
 test_client = TestClient(app)
+# Header variables
 login_token = ""
 admin_token = ""
+
+piece_created_id = None
+exposicion_id = None
 
 ## user tests
 def test_create_user_fail():
@@ -32,6 +37,7 @@ def test_login_admin():
 def test_create_user():
     assert admin_token
     response = test_client.post("/user", data=CREATE_USER, headers={"Authorization": admin_token})
+    print(response)
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["username"] == "test_reader"
@@ -70,23 +76,127 @@ def test_delete_user():
     query_parameters = f"?username={CREATE_USER['username']}"
     response = test_client.get("/user/" + query_parameters)
     assert response.json()
-    db.delete_user(CREATE_USER["username"])
+    db.delete_node_by_id_key(["user"], "username", CREATE_USER["username"])
     response = test_client.get("/user/" + query_parameters)
     assert not response.json()
 
 ## main test
 def test_create_piece_full():
+    global piece_created_id
     response = test_client.put("/add-piece", headers={"Authorization": admin_token}, data=CREATE_PIECE_FORM, files=None)
+    print(response.json())
     assert response.status_code == 200
     data = response.json()
+    piece_created_id = data[0]['id']
     assert len(data) == 2
-    assert data[0]['p']['id'] == CREATE_PIECE_BODY['id']
-    assert { "funcion": "Simbólica-utilitaria", "nombre_comun": "Alfiler", "nombre_especifico": "Ketawue"} == data[1]['c']
+    assert data[0]['pieza']['id'] == CREATE_PIECE_BODY['properties']['id']
+    assert data[1]['componente'] == CREATE_PIECE_BODY['components'][0]['properties']
 
 def test_create_component():
-    response = test_client.put("/add-component", headers={"Authorization": admin_token}, data=CREATE_PIECE_FORM, files=None)
+    CREATE_COMPONENT_FORM['piece_id'] = piece_created_id
+    print(CREATE_COMPONENT_FORM)
+    response = test_client.put("/add-component", headers={"Authorization": admin_token}, data=CREATE_COMPONENT_FORM, files=None)
+    print(response.json())
+    assert response.status_code == 200
+
+def test_get_piezas_no_filters():
+    response = test_client.post("/pieces", headers={"Authorization": admin_token}, json={})
+    print(response.json())
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) <= 50
+
+def test_get_piezas_with_match_id_filter():
+    response = test_client.post("/pieces", headers={"Authorization": admin_token}, json=FILTERS_0, files=None)
+    print(response.json())
+    assert response.status_code == 200
+    data = response.json()
+    assert data[1]['count'] == 1
+
+def test_get_piezas_with_multi_no_intersection_filters():
+    response = test_client.post("/pieces", headers={"Authorization": admin_token}, json=FILTERS_1, files=None)
+    print(response.json())
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) <= 50
+
+def test_get_piezas_with_single_filter():
+    response = test_client.post("/pieces", headers={"Authorization": admin_token}, json=FILTERS_1, files=None)
+    print(response.json())
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) <= 50
+
+def test_create_exposicion():
+    request_body = {
+        "piezas_element_ids": [piece_created_id],
+        "properties": {
+            "name": "Sellos de excelencia a la Artesanía Chile. 10 años de creación",
+            "fecha": "Noviembre de 2018 - marzo 2019",
+            "lugar_de_exposicion": "Sala MAPA/GAM",
+        },
+    }
+    response = test_client.put("/exposicion", headers={"Authorization": admin_token}, json=request_body)
+    assert response.status_code == 200
+    response = test_client.get("/nodes")
+
+def test_get_piece_and_components():
+    response = test_client.get("/components", headers={"Authorization": admin_token}, params={"piece_id": piece_created_id})
+    assert response.status_code == 200
+    piece, components = response.json()
+    assert len(components) == 2
+    assert piece["exposicion"]["name"] == "Sellos de excelencia a la Artesanía Chile. 10 años de creación"
+    assert any([c["ubicacion"] for c in components])
+    
+
+def test_delete_pieza():
+    response = test_client.delete("/pieces", headers={"Authorization": admin_token}, params={"node_id": "12345"})
+    print(response.json())
+    assert response.status_code == 200
+
+def test_delete_exposicion():
+    response = test_client.delete("/nodes/id", headers={"Authorization": admin_token}, 
+                                  params={"label": 'exposicion',
+                                          'key': 'name',
+                                          'val': 'Sellos de excelencia a la Artesanía Chile. 10 años de creación'
+                                        })
+    print(response.json())
+    assert response.status_code == 200
+
+def test_create_pieza_with_image():
+    files_count = len(os.listdir(UPLOAD_DIR))
+    with open(os.path.join("test", "apple.jpg"), "rb") as f:
+        print(f)
+        files = [("images", f)]
+        response = test_client.put("/add-piece", headers={"Authorization": admin_token},
+                                data=CREATE_PIECE_FORM,
+                                files=files
+                                )
+    print(response.json())
     assert response.status_code == 200
     data = response.json()
     assert len(data) == 2
-    assert data[0]['p']['id'] == CREATE_PIECE_BODY['id']
-    assert { "funcion": "Simbólica-utilitaria", "nombre_comun": "Alfiler", "nombre_especifico": "Ketawue"} == data[1]['c']
+    assert data[0]['pieza']['id'] == CREATE_PIECE_BODY['properties']['id']
+    assert data[1]['componente'] == CREATE_PIECE_BODY['components'][0]['properties']
+    new_files_count = len(os.listdir(UPLOAD_DIR))
+    assert new_files_count == files_count + 1
+
+def test_delete_pieza_with_image():
+    files_count = len(os.listdir(UPLOAD_DIR))
+    response = test_client.delete("/pieces", headers={"Authorization": admin_token}, params={"node_id": "12345"})
+    print(response.json())
+    assert response.status_code == 200
+    new_files_count = len(os.listdir(UPLOAD_DIR))
+    assert new_files_count == files_count - 1
+
+def test_get_non_existant_label_nodes():
+    response = test_client.get("/nodes", headers={"Authorization": admin_token}, params={"labels": ['made_up_label']})
+    assert response.status_code == 422
+
+def test_get_no_intersection_labels_nodes():
+    response = test_client.get("/nodes", headers={"Authorization": admin_token}, params={"labels": ['pieza', 'exposicion']})
+    data = response.json()
+    print(data)
+    assert response.status_code == 200
+    assert not data
+
